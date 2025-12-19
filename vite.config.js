57bc3c978dd8d635eb258ffb4b5a4c299a1c95b3
zalwagdani <71978382+zalwagdani@users.noreply.github.com>
2025-12-19 17:48:36 +0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Media/' : '/',
-  // Repository name: Media
+  base: '/media-page/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
